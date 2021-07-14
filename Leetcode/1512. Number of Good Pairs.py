4c78200b156b733c4726ac8f8f7236672060362a
vampirepapi <shubhamsourabh8@gmail.com>
2021-07-14 21:56:57 +0530

    1365,1512

---
+++ Leetcode/1512. Number of Good Pairs.py
@@ -0,0 +1,24 @@
+Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count=0
+        for i in range(len(nums)):
+	        for j in range(i+1,len(nums)):
+                
+		        if nums[i] == nums[j]:
+			        count+=1
+        return count
+        
--- Leetcode/tryRandom.py
+++ Leetcode/tryRandom.py
@@ -1,4 +1,18 @@
-names = ['Harry', 'Emma', 'John']
-ages = [19, 20, 18]
-## printing names and ages correspondingly using zip()
-print(list(zip(ages,names)))
+nums = [8,1,2,2,3]
+count=0
+# for x in range (len(nums)):
+#     for j in range(x+1,len(nums)):
+#         if nums[x] > nums[j]:
+#             count+=1
+#         else:
+#             count += 0
+# print(count)
+ans = []
+print(nums)
+sort = sorted(nums)
+print(sort)
+for i in nums:
+    l=(sort.index(i))
+    print(l)
+    ans.append(l)
+print(ans)
