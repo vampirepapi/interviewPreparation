6655bd02272d1f9084e3634fc7b48b66540abf47
vampirepapi <shubhamsourabh8@gmail.com>
2021-07-15 23:25:02 +0530

    pta nahi ky h yeh

--- Leetcode/tryRandom.py
+++ Leetcode/tryRandom.py
@@ -1,18 +1,8 @@
-nums = [8,1,2,2,3]
-count=0
-# for x in range (len(nums)):
-#     for j in range(x+1,len(nums)):
-#         if nums[x] > nums[j]:
-#             count+=1
-#         else:
-#             count += 0
-# print(count)
-ans = []
-print(nums)
-sort = sorted(nums)
-print(sort)
-for i in nums:
-    l=(sort.index(i))
-    print(l)
-    ans.append(l)
-print(ans)
+l=[]
+k=[]
+m=[]
+
+arr=[0,2,1,2,2,1,0,0,2,2]
+
+for x in arr:
+    print(x)
