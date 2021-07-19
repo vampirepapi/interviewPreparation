80e784485b48763a84dcd1503341a5766c33dba6
vampirepapi <shubhamsourabh8@gmail.com>
2021-07-20 02:02:58 +0530

    added more files

--- dsa_450/sort array of 0 1 2.py
+++ dsa_450/sort array of 0 1 2.py
@@ -1,3 +1,4 @@
+#Python Specific
 def sort0(arr):
   l=[]
   m=[]
@@ -11,3 +12,52 @@ def sort0(arr):
       n.append(x)
   return l + m + n
 print(sort0([0,2,2,1,1,0,2,1]))
+
+#TC =O(n)
+#SC = O(n)
+
+
+#General Approach
+def Sort012(arr):
+  countZero=0
+  countOne=0
+  countTwo=0
+
+  idx=0
+  while idx<len(arr):
+    if arr[idx] == 0:
+      countZero+=1
+
+    if arr[idx] == 1:
+      countOne+=1
+
+    if arr[idx] == 2:
+      countTwo+=1
+
+    idx+=1
+
+  countK=0
+  popArr=[None] * len(arr)
+
+  for x in range(countZero):
+    popArr[countK] = 0
+    countK+=1
+
+  for x in range(countOne):
+    popArr[countK] = 1
+    countK+=1
+
+  for x in range(countTwo):
+    popArr[countK] = 2
+    countK+=1
+  
+  return popArr
+
+print(Sort012([0,2,2,1,1,0,2,1]))
+
+#TC =O(n)
+#SC = O(n)
+
+
+#For more optimization we can use :
+      #Dutch National Flag Problem
