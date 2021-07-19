d184de6d443c9d8b6bbff02503ef8cee28b6a1d3
vampirepapi <shubhamsourabh8@gmail.com>
2021-07-19 17:00:28 +0530

    added gcd

--- tryRandom.py
+++ tryRandom.py
@@ -1,13 +1,38 @@
-def sort0(arr):
-  l=[]
-  m=[]
-  n=[]
-  for x in arr:
-    if x == 0:
-      l.append(x)
-    elif x == 1:
-      m.append(x)
+#Naive Approach:-
+def gcd(a, b):
+  if a>b:
+    small = b
+  else:
+    small = a
+  while small>0:
+    if a%small == 0 and b%small == 0:
+      break
+    small-=1
+  return small 
+print(gcd(4, 6))
+
+#TC= O(min(a,b))
+
+# Euclidean Algorithm:
+# if b is smaller than a ,
+# gcd(a,b) = gcd(a-b,b)
+
+#Basic Eucledian Approach:-
+def gcd(a,b):
+  while a!=b:
+    if a>b:
+      a=a-b
     else:
-      n.append(x)
-  return l + m + n
-print(sort0([0,2,2,1,1,0,2,1]))
+      b=b-a
+  return a
+print(gcd(4, 6))
+
+#Optimized Eucledian Approach(Best Approach):-
+def gcd (a,b):
+  if b==0:
+    return a
+  else:
+    return gcd(b, a%b)
+
+print(gcd(4, 20))
+  
