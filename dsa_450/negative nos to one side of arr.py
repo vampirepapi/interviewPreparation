dc7ac1db721e60c88f45d1caa03f8f42cc3aff6c
vampirepapi <shubhamsourabh8@gmail.com>
2021-07-20 22:51:58 +0530

    negative nos to one side of arr done will aded genearl solution later

---
+++ dsa_450/negative nos to one side of arr.py
@@ -0,0 +1,24 @@
+def qsort(arr):
+	if len(arr) <= 1:
+		return arr
+	else:
+		pivot = arr.pop()
+
+	items_greater = []
+	items_lower = []
+
+	for x in arr:
+		if x > pivot:
+			items_greater.append(x)
+
+		if x < pivot:
+			items_lower.append(x)
+
+	#move all -ve nos to the end of arr
+	#return qsort(items_greater) + [pivot] + qsort(items_lower) 
+	
+	#move all -ve nos to the start of arr
+	return qsort(items_lower) + [pivot] + qsort(items_greater)  
+
+
+print(qsort([-1, 2, -3, 4, 5, 6, -7, 8, 9]))
