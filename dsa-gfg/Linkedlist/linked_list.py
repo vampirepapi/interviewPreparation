6ac2a775e1d67cf20047b91b03863617fc5c15d6
vampirepapi <shubhamsourabh8@gmail.com>
2021-08-11 16:49:39 +0530

    remove data from the given index

--- dsa-gfg/Linkedlist/linked_list.py
+++ dsa-gfg/Linkedlist/linked_list.py
@@ -1,3 +1,4 @@
+#YT - Link = https://www.youtube.com/watch?v=qp8u-frRAnU&t=633s&ab_channel=codebasics
 class Node:
 	def __init__(self, data = None, next = None):
 		self.data = data 
@@ -7,6 +8,8 @@ class Linkedlist:
 	def __init__(self):
 		self.head = None 
 
+
+	#Inserting data at the begining
 	def insert_at_begining(self, data):
 		node = Node(data, self.head)
 		self.head = node 
@@ -20,11 +23,13 @@ class Linkedlist:
 		llstr = ''
 
 		while itr:
-			llstr += str(itr.data) + '-->'
+			llstr += str(itr.data) + '-->' if itr.next else str(itr.data)
 			itr = itr.next
 
 		print(llstr) 
 
+
+	#Inserting data at the end
 	def insert_at_end(self,data):
 		if self.head is None:
 			node = Node(data, None)
@@ -39,13 +44,46 @@ class Linkedlist:
 
 		itr.next = Node(data, None)
 
+
+	#Inserting list of values as input
 	def insert_values(self, data_list):
 		self.head = None
 
 		for data in data_list:
 			self.insert_at_end(data)
 
+	
+	#length of the linked list
+	def get_length(self):
+		count = 0
 
+		itr = self.head
+
+		while itr:
+			count+=1
+			itr = itr.next
+
+		return count
+
+
+	#Remove from given index
+	def remove_at(self, index):
+		if index < 0 or index >= self.get_length():
+			raise Exception("invalid index")
+
+		if index == 0:
+			self.head = self.head.next
+			return
+
+		count = 0
+		itr = self.head
+
+		while itr:
+			if count == index-1:
+				itr.next = itr.next.next
+				break
+
+			itr = itr.next
 
 
 
@@ -58,4 +96,7 @@ if __name__ == '__main__':
 	# ll.insert_at_begining(54)
 	ll.insert_values([1,2,3,4,5,6])
 	# ll.insert_values(['shubham','anish','himanshu'])
+	#ll.print()
+	print("Length:", ll.get_length())
+	ll.remove_at(1)
 	ll.print()
