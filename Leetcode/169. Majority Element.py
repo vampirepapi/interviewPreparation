3b66e9d6f9d8cc4d6e899e6ff9b61bb8bb560bef
vampirepapi <shubhamsourabh8@gmail.com>
2021-10-28 05:35:05 +0530

    abra ca dabra

---
+++ Leetcode/169. Majority Element.py
@@ -0,0 +1,33 @@
+# 169. Majority Element
+# Easy
+
+# 6859
+
+# 294
+
+# Add to List
+
+# Share
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+#Solution1
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums)//2]
+
+p=Solution()
+print(p.majorityElement([3,2,3]))
--- Leetcode/88. Merge Sorted Array.py
+++ Leetcode/88. Merge Sorted Array.py
@@ -59,15 +59,15 @@ class Solution:
         """
         last_idx = m+n-1
         while m>0 and n>0:
-            if nums1[m] > nums2[n]:
-                nums1[last_idx] = nums1[m]
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last_idx] = nums1[m-1]
                 m-=1
             else:
-                nums1[last_idx] = nums2[n]
+                nums1[last_idx] = nums2[n-1]
                 n-=1
             last_idx-=1
 
 
         while n>0:
-            nums1[last_idx] = nums2[n]
+            nums1[last_idx] = nums2[n-1]
             n, last_idx = n-1, last_idx-1
