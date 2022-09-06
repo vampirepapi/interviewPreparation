fb5f7eb96ca8a4fa70cfe49e1d30cb730f156ebd
Shubham Sourabh <39551118+vampirepapi@users.noreply.github.com>
2022-09-06 10:36:42 +0530

    Delete 217-contains-duplicate directory

--- 217-contains-duplicate/217-contains-duplicate.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        noDup=set(nums)
-        if len(noDup) != len(nums):
-            return True;
-        return False;
